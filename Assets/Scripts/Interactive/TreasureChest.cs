95515f029c8ff37c65c266229d28b9a3a2ffd768
Michael Pan <panmpan@gmail.com>
2022-12-21 01:26:33 +0800

    增加寶箱動畫

--- Assets/Scripts/Interactive/TreasureChest.cs
+++ Assets/Scripts/Interactive/TreasureChest.cs
@@ -11,6 +11,8 @@ public class TreasureChest : MonoBehaviour
     private ValueWithEnable<int> coreGain;
     [SerializeField]
     private Inventory playerInventory;
+    [SerializeField]
+    private Animator animator;
 
     private bool _opened;
 
@@ -21,11 +23,23 @@ public class TreasureChest : MonoBehaviour
 
         _opened = true;
 
+        StartCoroutine(C_Animate());
+    }
+
+    public IEnumerator C_Animate()
+    {
+        animator.enabled = true;
+
+        WaitForSeconds miliSecond = new WaitForSeconds(0.1f);
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0)
+        {
+            yield return miliSecond;
+        }
+
+        // TODO: Show inventory UI
         if (appleGain.Enable)
             playerInventory.ChangeAppleCount(appleGain.Value);
         if (coreGain.Enable)
             playerInventory.ChangeCoreCount(coreGain.Value);
-
-        gameObject.SetActive(false);
     }
 }
