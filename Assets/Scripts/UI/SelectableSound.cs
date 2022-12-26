31acac22b4c939d0b4d21cd432cf316e9e3854a8
Michael Pan <panmpan@gmail.com>
2022-12-26 10:46:08 +0800

    UI聲音會被設定影響

--- Assets/Scripts/UI/SelectableSound.cs
+++ Assets/Scripts/UI/SelectableSound.cs
@@ -26,33 +26,33 @@ public class SelectableSound : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnSelect(BaseEventData eventData)
     {
         if (soundPreset.OnPointerEnter)
-            s_audioSource.PlayOneShot(soundPreset.OnPointerEnter, soundPreset.OnPointerEnterVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnPointerEnter, soundPreset.OnPointerEnterVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
     public void OnDeselect(BaseEventData eventData)
     {
         if (soundPreset.OnPointerExit)
-            s_audioSource.PlayOneShot(soundPreset.OnPointerExit, soundPreset.OnPointerExitVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnPointerExit, soundPreset.OnPointerExitVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (soundPreset.OnSelect)
-            s_audioSource.PlayOneShot(soundPreset.OnSelect, soundPreset.OnSelectVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnSelect, soundPreset.OnSelectVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         if (soundPreset.OnDeselect)
-            s_audioSource.PlayOneShot(soundPreset.OnDeselect, soundPreset.OnDeselectVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnDeselect, soundPreset.OnDeselectVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (soundPreset.OnSubmit)
-            s_audioSource.PlayOneShot(soundPreset.OnSubmit, soundPreset.OnSubmitVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnSubmit, soundPreset.OnSubmitVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
     public void OnSubmit(BaseEventData eventData)
     {
         if (soundPreset.OnSubmit)
-            s_audioSource.PlayOneShot(soundPreset.OnSubmit, soundPreset.OnSubmitVolume);
+            s_audioSource.PlayOneShot(soundPreset.OnSubmit, soundPreset.OnSubmitVolume * AudioVolumeControl.ins.SFXVolumeSliderValue);
     }
 }
